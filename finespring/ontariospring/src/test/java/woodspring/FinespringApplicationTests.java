2002db3f24095d8d523b1414a3c2dc7dfff200c9
TT-DELL7010\Alfred <woodspring.toronto@gmail.com>
2017-04-30 22:47:57 -0400

    first commit

---
+++ finespring/ontariospring/src/test/java/woodspring/FinespringApplicationTests.java
@@ -0,0 +1,16 @@
+package woodspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinespringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
